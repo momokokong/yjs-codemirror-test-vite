c4ed501dff51c188dd9cf663c6fdb070b9095a71
momokokong <momokokong@gmail.com>
2024-08-29 23:38:40 -0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Editor } from './Components/Editor' 
+import './App.css'
+
+function App() {
+//   const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <Editor />
+    </>
+  )
+}
+
+export default App
